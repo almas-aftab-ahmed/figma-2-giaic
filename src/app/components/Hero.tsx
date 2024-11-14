773d971535ef053759536b8d4809105b7c7c42d3
almas-aftab-ahmed <almassiddiqui375@mail.com>
2024-11-13 22:55:51 -0800

    initial commit

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,18 +4,17 @@ import NavBar from './NavBar';
 const Hero = () => {
   return (
     <div
-     id='hero'
-      className="min-h-screen bg-no-repeat bg-[url(/cartoon.jpg)]  bg-cover"
-    style={{backgroundSize: "35%", backgroundPosition: "left 100px top 100px"}}>
-      <NavBar/>
-      <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-        <div className='hidden lg:block'></div>
-        <div className='text-[800px] sm:text-[100px] font-bold leading-tight flext-justify-center items-center'>
-            
-                <p data-aos="zoom-in-up">I&apos;m</p>
-                <p data-aos="zoom-in-up">Almas</p>
-                <p data-aos="zoom-in-up">Aftab</p>
-            
+      id="hero"
+      className="min-h-screen bg-no-repeat bg-[url(/cartoon.jpg)] bg-cover"
+      style={{ backgroundSize: "35%", backgroundPosition: "left 100px top 100px" }}
+    >
+      <NavBar />
+      <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)] items-center">
+        <div className="hidden lg:block"></div>
+        <div className="flex flex-col text-center lg:text-left justify-center items-center lg:items-start">
+          <p className="text-4xl sm:text-6xl lg:text-8xl font-bold leading-tight" data-aos="zoom-in-up">I'm</p>
+          <p className="text-4xl sm:text-6xl lg:text-8xl font-bold leading-tight" data-aos="zoom-in-up">Almas</p>
+          <p className="text-4xl sm:text-6xl lg:text-8xl font-bold leading-tight" data-aos="zoom-in-up">Aftab</p>
         </div>
       </div>
     </div>
